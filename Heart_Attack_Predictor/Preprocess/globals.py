6d62b57c9b71d878211229cd2014668f2d03e986
Wonjae Oh <wonjae@live.unc.edu>
2024-04-24 20:39:57 -0400

    .

--- Heart_Attack_Predictor/Preprocess/globals.py
+++ Heart_Attack_Predictor/Preprocess/globals.py
@@ -1,9 +1,9 @@
 # Excluding 'plan' from our list of categorical variables because there is linearity in its categories (1-99)
-RANDHIE_CATEGORICAL_VARIABLES = ['site', 'female', 'child', 'fchild', 'hlthg', 'hlthf', 'hlthp', 'tookphys']
+RANDHIE_CATEGORICAL_VARIABLES = ['coins', 'site', 'female', 'child', 'fchild', 'hlthg', 'hlthf', 'hlthp', 'tookphys']
 # Excluding 'plan' from list of numeric variables as well because it should not be standardized; it is a linear categorical variable
 # Also exclusing 'zper' since we don't use it for prediction and only use it for collapsing
 RANDHIE_NUMERIC_VARIABLES = ['black', 'mhi', 'coins', 'year', 'income', 'xage', 'educdec', 'time', 'outpdol', 'drugdol', 'suppdol', 'mentdol', 'inpdol', 'meddol', 'totadm', 'inpmis', 'mentvis', 'mdvis', 'notmdvis', 'num', 'disea', 'physlm', 'ghindx', 'mdeoff', 'pioff', 'lfam', 'lpi', 'idp', 'logc', 'fmde', 'xghindx', 'linc', 'lnum', 'lnmeddol', 'binexp']
 
 # heart_attack_prediction.csv variables
-HEART_CATEGORICAL_VARIABLES = []
-HEART_NUMERIC_VARIABLES = []
+HEART_CATEGORICAL_VARIABLES = ['Diabetes', 'Family History', 'Smoking', 'Obesity', 'Alcohol Consumption', 'Previous Heart Problems', 'Medication Use', 'Sex', 'Diet', 'Country']
+HEART_NUMERIC_VARIABLES = ['Age', 'Cholesterol', 'Heart Rate', 'Exercise Hours Per Week', 'Stress Level', 'Sedentary Hours Per Day', 'BMI', 'Triglycerides', 'Physical Activity Days Per Week', 'Sleep Hours Per Day']
